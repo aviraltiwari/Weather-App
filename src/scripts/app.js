bf9c28f6ed8b85f62ed4de1645c59ecac93fa65c
Aviral <aviraltiwarimgmt@gmail.com>
2022-06-07 12:37:38 +0530

    fixed  bugs

---
+++ src/scripts/app.js
@@ -0,0 +1,64 @@
+import config from "./config.js";
+
+const updateWeather = () => {
+    document.querySelector('.city-name').innerText = city;
+    document.querySelector('.temperature-value').innerText = data.main.temp
+    // console.log(data)
+    document.querySelector('.feels-like').innerText = data.main.feels_like;
+    document.querySelector('.humidity').innerText = data.main.humidity;
+}
+
+
+const weather = {
+    lat: 25.4358,
+    lon: 81.8463,
+    units: 'metric',
+    fetchWeather: function (city) {
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + this.lat + '&lon=' + this.lon + '&units=' + this.units + '&appid=' + config.openWeatherAPIKey)
+            .then((response) => response.json())
+            .then((data) => {
+                updateWeather(city, data.main.temp, data.main.feels_like, data.)
+            })
+            .catch((err) => console.log(err))
+    }
+
+}
+
+const autoSuggestion = {
+    url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/{search_text}.json',
+    getAutoComplete: function (search_text) {
+        fetch(this.url.replace('{search_text}', search_text) + '?access_token=' + config.mapBoxAccessToken)
+            .then((response) => response.json())
+            .then((data) => {
+                for (let i = 0; i < 5; i++) {
+                    let place = data.features[i];
+                    const element = document.createElement('li')
+                    element.innerText = place.place_name;
+                    const autosuggest = document.querySelector('.autosuggestions')
+                    element.addEventListener('click', () => {
+                        document.querySelector('.search-bar').value = place.place_name;
+                        weather.lat = place.center[1];
+                        weather.lon = place.center[0];
+                        weather.fetchWeather(place.place_name);
+                        autosuggest.innerText = ''
+                        const weatherSelector = document.querySelector('.weather')
+                        weatherSelector.style.display = 'block';
+                    })
+                    autosuggest.appendChild(element)
+                }
+            })
+            .catch((err) => {
+                const element = document.querySelector('.autosuggestions')
+                locationInput.value !== '' ? element.innerHTML = 'No results found' : element.innerHTML = '';
+            })
+    }
+}
+
+const locationInput = document.querySelector('.search-bar')
+locationInput.addEventListener('input', (e) => {
+    document.querySelector('.autosuggestions').innerHTML = '';
+    autoSuggestion.getAutoComplete(e.target.value)
+})
+locationInput.addEventListener('click', () => {
+    locationInput.value = ''
+})
