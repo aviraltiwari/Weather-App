6b705c2ac83998ebab2f938de110be4ed7b13c87
Aviral <aviraltiwarimgmt@gmail.com>
2022-06-07 13:00:52 +0530

    refactor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,30 @@
 import config from "./config.js";
 
-const updateWeather = () => {
+const updateWeather = (city, temp, feels_like, humidity) => {
     document.querySelector('.city-name').innerText = city;
-    document.querySelector('.temperature-value').innerText = data.main.temp
-    // console.log(data)
-    document.querySelector('.feels-like').innerText = data.main.feels_like;
-    document.querySelector('.humidity').innerText = data.main.humidity;
+    document.querySelector('.temperature-value').innerText = temp
+    document.querySelector('.feels-like').innerText = feels_like;
+    document.querySelector('.humidity').innerText = humidity;
 }
 
+const updateAutoSuggestion = (data) => {
+    for (let i = 0; i < 5; i++) {
+        let place = data.features[i];
+        const element = document.createElement('li')
+        element.innerText = place.place_name;
+        const autosuggest = document.querySelector('.autosuggestions')
+        element.addEventListener('click', () => {
+            document.querySelector('.search-bar').value = place.place_name;
+            weather.lat = place.center[1];
+            weather.lon = place.center[0];
+            weather.fetchWeather(place.place_name);
+            autosuggest.innerText = ''
+            const weatherSelector = document.querySelector('.weather')
+            weatherSelector.style.display = 'block';
+        })
+        autosuggest.appendChild(element)
+    }
+}
 
 const weather = {
     lat: 25.4358,
@@ -16,9 +33,7 @@ const weather = {
     fetchWeather: function (city) {
         fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + this.lat + '&lon=' + this.lon + '&units=' + this.units + '&appid=' + config.openWeatherAPIKey)
             .then((response) => response.json())
-            .then((data) => {
-                updateWeather(city, data.main.temp, data.main.feels_like, data.)
-            })
+            .then((data) => updateWeather(city, data.main.temp, data.main.feels_like, data.main.humidity))
             .catch((err) => console.log(err))
     }
 
@@ -29,24 +44,7 @@ const autoSuggestion = {
     getAutoComplete: function (search_text) {
         fetch(this.url.replace('{search_text}', search_text) + '?access_token=' + config.mapBoxAccessToken)
             .then((response) => response.json())
-            .then((data) => {
-                for (let i = 0; i < 5; i++) {
-                    let place = data.features[i];
-                    const element = document.createElement('li')
-                    element.innerText = place.place_name;
-                    const autosuggest = document.querySelector('.autosuggestions')
-                    element.addEventListener('click', () => {
-                        document.querySelector('.search-bar').value = place.place_name;
-                        weather.lat = place.center[1];
-                        weather.lon = place.center[0];
-                        weather.fetchWeather(place.place_name);
-                        autosuggest.innerText = ''
-                        const weatherSelector = document.querySelector('.weather')
-                        weatherSelector.style.display = 'block';
-                    })
-                    autosuggest.appendChild(element)
-                }
-            })
+            .then((data) => updateAutoSuggestion(data))
             .catch((err) => {
                 const element = document.querySelector('.autosuggestions')
                 locationInput.value !== '' ? element.innerHTML = 'No results found' : element.innerHTML = '';
